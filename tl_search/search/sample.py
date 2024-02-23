eed5510cd8adcbe77f248d6757da774cc4748816
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-02-23 13:19:59 -0600

    feat: add random sampler

--- tl_search/search/sample.py
+++ tl_search/search/sample.py
@@ -1,4 +1,5 @@
 from typing import Any
+import random
 
 from stable_baselines3 import SAC
 
@@ -6,13 +7,17 @@ from tl_search.envs.tl_parking import TLAdversarialParkingEnv
 
 
 def sample_obs(
-    env: TLAdversarialParkingEnv, model: SAC, num_samples: int
+    env: TLAdversarialParkingEnv,
+    model: SAC,
+    num_samples: int,
+    population_scale: float = 1.5,
 ) -> list[dict[str, Any]]:
     obs_list: list[dict[str, Any]] = []
 
+    num_collected: int = round(num_samples * population_scale)
     sample_counter: int = 0
 
-    while sample_counter < num_samples:
+    while sample_counter < num_collected:
         obs, _ = env.reset()
         while True:
             action = model.predict(obs, deterministic=True)[0]
@@ -30,6 +35,6 @@ def sample_obs(
             if terminated or truncated:
                 break
 
-    assert len(obs_list) == num_samples
+    obs_list_sampled: list[dict[str, Any]] = random.sample(obs_list, num_samples)
 
-    return obs_list
+    return obs_list_sampled
