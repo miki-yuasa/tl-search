d1ee13f331bf7f4a3b3d311d8f5e2e343bfe4cf2
Mikihisa Yuasa <mik0915yua@outlook.com>
2024-11-20 13:31:07 -0600

    chore: fix the batch script

--- batch/push_batch.sh
+++ batch/push_batch.sh
@@ -4,14 +4,15 @@
 #SBATCH --account=huytran1-ic
 #SBATCH --ntasks-per-node=24              # Number of task (cores/ppn) per node
 #SBATCH --job-name=tl_search_exh_push          # Job name
-#SBATCH --output=tl_search_exh_push_%j.out   # Output file name
-#SBATCH --error=tl_search_exh_push_%j.err    # Error file name
+#SBATCH --output=./batch_out/tl_search_exh_push_%j.out   # Output file name
+#SBATCH --error=./batch_out/tl_search_exh_push_%j.err    # Error file name
 #SBATCH --partition=IllinoisComputes-GPU
 #SBATCH --gres=gpu:A100:1
 
 # Load the necessary modules
 cd /projects/illinois/eng/aerospace/huytran1/myuasa2/git/tl-search
 module load anaconda/2023-Mar/3
+conda install -c conda-forge spot=2.11.6
 source /projects/illinois/eng/aerospace/huytran1/myuasa2/.cache/pypoetry/virtualenvs/tl-search-WyzSwqE--py3.10/bin/activate
 
 # Run the serial executable
