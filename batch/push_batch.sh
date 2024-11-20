40dfb8eaa21f181e653ba15643ef4d8e13a4789a
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-11-20 12:33:40 -0600

    feat: implement job batch

--- batch/push_batch.sh
+++ batch/push_batch.sh
@@ -3,9 +3,17 @@
 #SBATCH --account=myuasa2           # Replace "account_name" with an account available to you
 #SBATCH --nodes=1                        # Number of nodes
 #SBATCH --ntasks-per-node=24              # Number of task (cores/ppn) per node
-#SBATCH --mem-per-cpu=3375               # Memory per core (value in MBs)
+#SBATCH --mem-per-cpu=515500               # Memory per core (value in MBs)
+#SBATCH --job-name=tl_search_exh_push          # Job name
+#SBATCH --output=tl_search_exh_push_%j.out   # Output file name
+#SBATCH --error=tl_search_exh_push_%j.err    # Error file name
+#SBATCH --gres=gpu:A100:1                      # Number of GPUs
+#SBATCH --partition=IllinoisComputes-GPU
 
-#
-cd ${SLURM_SUBMIT_DIR}
+# Load the necessary modules
+cd /projects/illinois/eng/aerospace/huytran1/myuasa2/git/tl-search
+module load anaconda/2023-Mar/3
+source /projects/illinois/eng/aerospace/huytran1/myuasa2/.cache/pypoetry/virtualenvs/tl-search-WyzSwqE--py3.10/bin/activate
 
 # Run the serial executable
+/projects/illinois/eng/aerospace/huytran1/myuasa2/.cache/pypoetry/virtualenvs/tl-search-WyzSwqE--py3.10/bin/python /projects/illinois/eng/aerospace/huytran1/myuasa2/git/tl-search/scripts/main_tl_exhaustive_push.py
